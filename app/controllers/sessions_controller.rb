5575cbd02c94bc0bfebd918914c91093752ead00
Devan <devan.moylan@backyardlion.com>
2017-08-01 11:53:46 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(name: params[:user][:name], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+    if @user.password == @user.password_confirmation && !@user.password.empty? && !@user.password.nil?
+       @user.save
+       session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
